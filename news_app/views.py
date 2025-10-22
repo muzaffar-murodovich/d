15714bc48b9b6542d686654a0470aec04a3dfc4a
Muzaffar Murodovich <muzaffarmurodogli@gmail.com>
2025-10-22 21:51:19 +0500

    +

--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import News, Category
 # Create your views here.
+
+
+def news_list(request):
+    news_list = News.published.all()
+
+    context = {
+        'news_list': news_list,
+    }
+
+    return render(request, 'news/news_list.html', context)
+
+def news_detail(request, id):
+    news = get_object_or_404(News, id=id, status=News.Status.Published)
+    context = {
+        "news": news,
+    }
+    return render(request, 'news/news_detail.html', context)
--- news_project/urls.py
+++ news_project/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', include('news_app.urls')),
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
