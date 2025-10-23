93dbb131d09cacaa96f258460077a2cff587d6d3
Muzaffar Murodovich <muzaffarmurodogli@gmail.com>
2025-10-23 21:05:02 +0500

    +

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import news_list, news_detail
+from .views import news_list, news_detail, homePageView, contactPageView, categoryPageView, singlePageView
 
 urlpatterns = [
-    path('all/', news_list, name='all_news_list'),
-    path('<int:id>/', news_detail, name='news_detail_page'),
+    path('', homePageView, name='home_page'),
+    path('news/', news_list, name='all_news_list'),
+    path('news/<int:id>/', news_detail, name='news_detail_page'),
+    path('contact/', contactPageView, name='contact'),
+    path('category/', categoryPageView, name='category'),
+    path('single/', singlePageView, name='single'),
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -18,3 +18,24 @@ def news_detail(request, id):
         "news": news,
     }
     return render(request, 'news/news_detail.html', context)
+
+def homePageView(request):
+    news = News.published.all()
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'categories': categories
+    }
+    return render(request, 'news/home.html', context)
+
+def contactPageView(request):
+    context = {}
+    return render(request, 'news/contact.html', context)
+
+def categoryPageView(request):
+    context = {}
+    return render(request, 'news/category.html', context)
+
+def singlePageView(request):
+    context = {}
+    return render(request, 'news/single.html', context)
